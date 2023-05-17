42a3067c75f22aaa0fb779aaa351ec5f85302c53
Cjpotter10 <caleb10joseph@gmail.com>
2023-05-17 14:09:59 -0500

    adding files creatd by firebase to set up github deploys

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"os"
 
 	draftState "github.com/CJPotter10/sbs-drafts-api/draft-state"
+	"github.com/CJPotter10/sbs-drafts-api/leagues"
+	"github.com/CJPotter10/sbs-drafts-api/owner"
+	"github.com/CJPotter10/sbs-drafts-api/utils"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
@@ -18,18 +21,26 @@ func main() {
 		port = fromEnv
 	}
 
+	utils.NewDatabaseClient()
+
 	fmt.Printf("Starting up on http://localhost:%s\n", port)
 
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
 
-	r.Get("/", func( w http.ResponseWriter, r *http.Request) {
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
 
 	dr := &draftState.DraftResources{}
 	r.Mount("/drafts", dr.Routes())
+
+	lr := &leagues.LeagueResources{}
+	r.Mount("/league", lr.Routes())
+
+	or := &owner.OwnerResources{}
+	r.Mount("/owner", or.Routes())
 
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
